d8b8e4c63b30621a82a537b76da23a681c8ee639
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-11 11:38:33 +0900

    Square

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать программу принимающую число и выдающее его квадрат
+*/
+Console.Write("Enter your number, please: ");
+string number = Console.ReadLine();
+int num = int.Parse (number);
+int square = num * num;
+Console.WriteLine ($"Square of your number {number} is {square}");
+
